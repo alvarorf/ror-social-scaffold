e3e5e8e5443d2c757c3a9116c6e02ff74a240524
alvaro <aaruizf@unal.edu.co>
2021-02-10 00:44:12 -0500

    Solve most of the linter errors

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   it 'should fail if you try to create an empty like' do
-   expect(Like.new).to_not be_valid
+    expect(Like.new).to_not be_valid
   end
   it 'should be valid' do
     u = User.create(name: 'User_#2', password: '123456', email: '123@123.com')
