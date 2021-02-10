1c4dc45050cc6f90dfd105b89d8cbe356dbe806b
Jarfsoft <andresjex@hotmail.com>
2021-02-10 10:48:56 -0600

    add hide comments from unfriend users feature

---
+++ app/helpers/comment_helper.rb
@@ -0,0 +1,19 @@
+module CommentHelper
+  def filter_friends_comment(comment)
+    u = User.where('id = ?', Post.where('id = ?', comment.post_id).first.user_id).first
+    u.friends.each do |f|
+      return comment if f.id == comment.user_id
+    end
+    return nil
+  end
+
+  def show_comment(comment)
+    return [] if comment.nil?
+
+    a = []
+    a.push comment.user.name + ': '
+    a.push comment.content
+    a.push comment.created_at.strftime("%Y/%m/%d")
+    a
+  end
+end
