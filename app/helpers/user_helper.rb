a2691c87298d21873b580c42062b66d21f1ff6ec
Jarfsoft <andresjex@hotmail.com>
2021-02-11 12:15:41 -0600

    fix linters, add screenshot to README file

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,23 +1,21 @@
 module UserHelper
   def show_invite_link(user)
-    unless current_user.friends.include? user
-      set_new_friend(user)
-      link_to 'Send friendship request', new_friendship_path
-    end
+    return if current_user.friends.include? user
+
+    set_new_friend(user)
+    link_to 'Send friendship request', new_friendship_path
   end
 
-  # def set_new_friend(user)
-  #  @new_friend = user
-  #  puts "Hello #{@new_friend}"
-  # end
   def check_if_pending(_user)
     Friendship.where('user_id = ?', current_user.id)
   end
 
   def toggle_friend_request(user)
-    if current_user != user
-      link_to 'Add as friend',
-              user_friendships_path(current_user, friendship: { friend_id: user.id, confirmed: false }), method: :post, class: 'btn-1 green_btn'
-    end
+    return unless current_user != user
+
+    link_to 'Add as friend',
+            user_friendships_path(current_user,
+                                  friendship: { friend_id: user.id,
+                                                confirmed: false }), method: :post, class: 'btn-1 green_btn'
   end
 end
