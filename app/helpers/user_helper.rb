71bbe763e057a274faab3f2a370224749fe550c1
Jarfsoft <andresjex@hotmail.com>
2021-02-15 16:52:27 -0600

    fix repeated request buttons

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -11,7 +11,7 @@ module UserHelper
   end
 
   def toggle_friend_request(user)
-    return if current_user == user || current_user.friends.include?(user)
+    return if current_user == user || current_user.friends.include?(user) || current_user.pending_friends.include?(user) || user.pending_friends.include?(current_user)
 
     link_to 'Add as friend',
             user_friendships_path(current_user,
@@ -21,11 +21,11 @@ module UserHelper
 
   def accept_friend_request(user)
     f = Friendship.find_by(user_id: user.id)
-    return if f.nil? || current_user.friends.include?(user)
+    return if f.nil? || current_user.friends.include?(user) || f.user_id == current_user.id
 
     link_to 'Accept as friend',
-            user_friendship_path(current_user, id: f.id, confirmed: true,
-                                  friendship: { friend_id: user.id, confirmed: true}), method: :put, class: 'btn-1 green_btn'
+            user_friendship_path(user, id: f.id, confirmed: true,
+                                  friendship: { friend_id: current_user.id, confirmed: true}), method: :put, class: 'btn-1 green_btn'
   end
 
   def list_friend_request(user)
