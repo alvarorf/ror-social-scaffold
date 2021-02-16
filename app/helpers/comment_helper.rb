d6c9c2fb0df6bfb09205df860a07a2794642c661
Jarfsoft <andresjex@hotmail.com>
2021-02-16 16:55:55 -0600

    fix comment issue

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -1,24 +1,15 @@
 module CommentHelper
   def filter_friends_comments(comment)
     u = User.where('id = ?', Post.where('id = ?', comment.post_id).first.user_id).first
+    d = ''
     u.friends.each do |f|
-      next unless f.id == comment.user_id || current_user.id == comment.user_id
+      next unless f.id != comment.user_id || current_user.id == comment.user_id
 
       a = "<p class=\"comment-data\">Made by: #{User.find_by(id: comment.user_id).name}</p>"
       b = "<p  class=\"comment-data\">#{comment.content}</p>"
       c = "<p class=\"comment-data\"> Made at : #{comment.created_at.strftime('%Y/%m/%d')}</p>"
       d = b + a + c
-      return d.html_safe
     end
-  end
-
-  def show_comment(comment)
-    return [] if comment.nil?
-
-    a = []
-    a.push "#{comment.user.name}: "
-    a.push comment.content
-    a.push comment.created_at.strftime('%Y/%m/%d')
-    a
+    d.html_safe
   end
 end
